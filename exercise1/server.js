53c10b4c0ffbc17d713b5e61312e15662d9bf07d
Nguyen Truong Duy <1510486@hcmut.edu.vn>
2018-03-16 10:25:19 +0700

    Update

--- exercise1/server.js
+++ exercise1/server.js
@@ -1,63 +1,60 @@
 const express = require('express');
 const hbs = require ('hbs');
+const bodyParser = require('body-parser');
+
+var urlencodedParser = bodyParser.urlencoded({ extended: true });
 
 var app = express();
-
-const yargs = require('yargs');
 
 const distance = require('./distance/distance');
 
 const geocode = require('./geocode/geocode');
 
 
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
 
-app.get('/api/geo', function(req, res) {
-  var lat = req.param('lat');
-  var lng = req.param('lng');
-
+app.get('/geocode', function(req, res) {
+  
+  res.render('geocode.hbs', {
+    pageTitle: 'Geocode'
+  });  
+  
+});
+app.post('/geocode/result', urlencodedParser, function (req, res){
+  var lat = req.body.lat;
+  var lng = req.body.lng;
   var location = {
       lat:lat,
       lng:lng
-    };
+  };
   console.log(location);
   geocode.geocodeAddress(location, (errorMessage, results) => {
       if (errorMessage) {
         console.log(errorMessage);
         res.send(errorMessage);
       } else {
-        //console.log(JSON.stringify(results, undefined, 2));
         res.setHeader('Content-Type', 'application/json');
         res.send(results);
       }
-    });
-  
-});
+    }); 
+ });
 
-app.set('view engine', 'hbs');
-app.use(express.static(__dirname + '/public'));
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home Page'
+  });
+});
 
 app.get('/api/distance', (req, res) => {
-  var lat1 = req.param('lat1');
-  var lng1 = req.param('lng1');
-  var lat2 = req.param('lat2');
-  var lng2 = req.param('lng2');
-  var dis = distance.getDistance(1.1,1.2, 5, 50);
-  res.send(`distance: ${dis}`);
+  var lat1 = req.query.lat1;
+  var lng1 = req.query.lng1;
+  var lat2 = req.query.lat2;
+  var lng2 = req.query.lng2;
+  var dis = distance.getDistance(lat1,lng1, lat2, lng2);
+  res.setHeader('Content-Type', 'application/json');
+  res.send({"distance(km)": dis});
 });
-
-// app.get('/about', (req, res) => {
-//   res.render('about.hbs', {
-//     pageTitle: 'About Page',
-//     currentYear: new Date().getFullYear()
-//   });
-// });
-
-// // /bad - send back json with errorMessage
-// app.get('/bad', (req, res) => {
-//   res.send({
-//     errorMessage: 'Unable to handle request'
-//   });
-// });
 
 app.listen(3000, () => {
   console.log('Server is up on port 3000');
